abe594bc0933db7adff700ecad3b37f5a6478fa9
Eugen Pirogoff <eugenpirogoff@me.com>
2013-07-26 14:29:50 +0200

    testing 7

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-Pluto
-=====
+Node.js on OpenShift
+====================================================================
+This package includes a dynamic Node.js build stage that will provide your application with a customized Node.js runtime.
+The version of Node that is available will depend on the requirements listed in your application's `package.json` file.
 
-websockets remote controller for node.js
+See: `.openshift/action_hooks/` for more informaiton on how the OpenShift build process works.
+
+Basic Setup
+-----------
+
+If this is your first time using OpenShift Online or Node.js, you'll have some quick prep-work to do:
+
+1. [Create an OpenShift Online account](http://openshift.redhat.com/app/account/new)
+2. If you don't already have the rhc (Red Hat Cloud) command-line tools, run: `sudo gem install rhc`
+3. Run `rhc setup` to link your OpenShift Online account with your local development environment, and to select an application namespace
+4. [Download and install Node.js](http://nodejs.org) for use in your local development environment: http://nodejs.org
+
+If you need any additional help getting started, these links may come in handy:
+
+ * https://openshift.redhat.com/community/get-started#cli
+ * https://openshift.redhat.com/community/developers/rhc-client-tools-install
+
+Host your Node.js applications on OpenShift
+-------------------------------------------
+
+Create a Node.js application.  This example will produce an application named **nodeapp**:
+
+    rhc app create nodeapp nodejs --from-code=git://github.com/ryanj/nodejs-custom-version-openshift.git
+
+The above example will output a folder named after your application which contains your local development source.  Make sure to run it from within a directory where you would like to store your development code.
+
+That's it!  You should be able to access your application at:
+
+    http://nodeapp-$yournamespace.rhcloud.com
+
+If your app requires a specific version of Node.js, just update the 'engines' section of your app's `package.json` file to specify your runtime requirements:
+
+    "engines": {
+        "node": ">= 0.10.0",
+        "npm": ">= 1.0.0"
+     },
+
+Commit your changes locally:
+
+    git add package.json
+    git commit -m 'updating package.json to select Node.js version 0.8.21'
+
+Then push your updates to OpenShift
+
+    git push
+
+Additional updates can be made via the same `git add`, `git commit`, and `git push` workflow.
--- server.js
+++ server.js
@@ -1,43 +1,3 @@
-/**
-* Pluto - Websocket Remote for Node.js
-* Eugen Pirogoff
-* web: http://www.eugenpirogoff.de
-* mail: eugenpirogoff@me.com
-**/
-
-
-// console.log("Node Version running : "+process.versions.node)
-
-// var http = require('http'), /* HTTP Server */
-//     express = require('express'), /* Express Framework */
-//     routes = require('./routes'); /* routes folder on filesystem, to know what to do with routing */
-
-// var app = express(); /* creating app from framework */
-// var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; /* fetching Port from enviroment or taking 3000*/
-// var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-// var server = app.listen(port, function(){
-//     console.log("App listening on Port : "+port); /* starting server on port 3000  or enviroment Port*/
-// }); 
-// var io = require('socket.io').listen(server); /* var io for listening for sockets on port 3000 on sever */
-
-// app.configure(function (){
-//     app.set('views', __dirname + '/views'); /*looking for jade templates in view folder*/
-//     app.set('view engine', 'jade'); /* seting jade for templating engine*/
-//     app.use(express.static(__dirname + '/public')); /*here goes our static content, games etc*/
-//     app.use(express.logger('dev')); /* Logging for Development Cycle*/
-//     app.use(express.bodyParser());
-//     app.use(app.router);
-// });
-
-// app.get('/', routes.index); /* Basic Routing for '/' to the index.jade template inside the index.js */
-
-// app.configure('development', function(){
-//   app.use(express.errorHandler());
-// });
-
-
-
-
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
@@ -51,7 +11,7 @@ var SampleApp = function() {
 
     //  Scope.
     var self = this;
-    var versions = process.versions.node
+
 
     /*  ================================================================  */
     /*  Helper functions.                                                 */
@@ -83,7 +43,7 @@ var SampleApp = function() {
         }
 
         //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./public/index.html');
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
     };
 
 
@@ -134,10 +94,6 @@ var SampleApp = function() {
      */
     self.createRoutes = function() {
         self.routes = { };
-
-        self.routes['/version'] = function(req, res) {
-            res.send(versions);
-        };
 
         // Routes for /health, /asciimo, /env and /
         self.routes['/health'] = function(req, res) {
