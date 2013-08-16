1be7345e85446d5f0fd98d0dbf72a23a66aeb298
Eugen Pirogoff <eugenpirogoff@me.com>
2013-08-16 03:07:36 +0200

    TLD=pluto.jit.su, landingpage ready

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
-var eugen = null;
+io = io.connect(document.domain)
+
+var pluto_id = '000000';
+var comet = {}
+comet['pluto_id']=pluto_id
+
+io.emit('pluto_data', comet)
+
+io.on('pluto_relay', function(data){
+	$('.pluto_client_emitter').html(data['pluto_data'])
+})
